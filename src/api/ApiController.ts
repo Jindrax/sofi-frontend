bcd71aa284ac8a56829abf15f9ba732e95b66eb7
Jairo Vanegas <jairo.vanegas@javeriana.edu.co>
2022-05-29 02:58:13 -0500

    Login Actualizado

--- src/api/ApiController.ts
+++ src/api/ApiController.ts
@@ -7,6 +7,8 @@ export class ApiController {
   private static url: string = process.env.NODE_ENV==="development"? "http://localhost:443" : location.protocol + "//" + location.hostname + (location.port? ":" + location.port : "");
 
   static async post(path: string, data: any) {
+    console.log("ENV: " + process.env.NODE_ENV);
+    console.log("URL:" + ApiController.url);
     return await axios.post(ApiController.url + path, data, ApiController.userStore.token !== "" ? {
       headers: {
         Authorization: "Bearer " + ApiController.userStore.token
